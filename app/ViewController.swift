bdd085ed545634504fac0e7b79f455c23e6847ee
azzaz <azzaz@azzazs-Mac.local>
2018-04-30 00:37:06 +0200

    First Uploading

--- app/ViewController.swift
+++ app/ViewController.swift
@@ -10,6 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textFieldFirstName: UITextField!
+    
+    
+    @IBOutlet weak var textFieldLastName: UITextField!
+    
+    @IBOutlet weak var textFieldEmail: UITextField!
+   
+    @IBOutlet weak var textFieldPassword: UITextField!
+    
+    @IBOutlet weak var Label1: UILabel!
+    
+    
+    @IBAction func regist(_ sender: Any) {
+        
+        if !((textFieldFirstName.text?.isEmpty)!) &&
+            !((textFieldLastName.text?.isEmpty)!) &&
+            !((textFieldEmail.text?.isEmpty)!   ) &&
+            !((textFieldPassword.text?.isEmpty)!)
+        {
+            performSegue(withIdentifier: "register", sender: self)
+        }
+        else
+        {
+            Label1.text = "Plz Enter all textFields"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
